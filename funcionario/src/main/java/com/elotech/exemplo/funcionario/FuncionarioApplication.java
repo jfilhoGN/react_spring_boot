e50f0cbf4a4bc83badb52a44261d416b4f05bc0a
João Martins Filho <joao.martins@elotech.com.br>
2018-09-11 11:22:41 -0300

    add project spring_boot

---
+++ funcionario/src/main/java/com/elotech/exemplo/funcionario/FuncionarioApplication.java
@@ -0,0 +1,12 @@
+package com.elotech.exemplo.funcionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuncionarioApplication.class, args);
+    }
+}
